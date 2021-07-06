ec33df90101b0b2fd9f87d14d2c39b70b6c60369
Dennis <11572625+dke85@users.noreply.github.com>
2021-07-06 19:42:17 -0400

    Made Bunny class based on interface and created a Basic Bunny. Tested that the base count and spawn multiplier works but with rounding problem. Added test output while loop

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 
+
 namespace BunnyTakeover
 
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
-            Console.WriteLine("Hello World!");
+            Bunny BasicBun = new Bunny(500, 1.0);
+            Console.WriteLine($"Bunny Multiplier: {BasicBun.Spawn}");
+            Console.WriteLine($"Bunny Base: {BasicBun.Count}");
+
+            while (BasicBun.Count < 7874965825){
+                Console.Write($"Current Buns:{BasicBun.Count}, ");
+                BasicBun.Count = BasicBun.Count + (BasicBun.Count*(long)Math.Round(BasicBun.Spawn));
+                Console.Write($"increased to: {BasicBun.Count} \n");
+            }
         }
     }
 }
