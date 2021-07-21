90d74810dd0ee880919c7504499bff1cbac01aea
Dennis <11572625+dke85@users.noreply.github.com>
2021-07-20 20:40:58 -0400

    Improved bunny loop and added super bunny

--- Program.cs
+++ Program.cs
@@ -8,14 +8,37 @@ namespace BunnyTakeover
     {
         static void Main()
         {
+            long BunTotal = 0;
+            
             Bunny BasicBun = new Bunny(500, 1.0);
+            Bunny SuperBun = new Bunny(500, 5.0);
+
             Console.WriteLine($"Bunny Multiplier: {BasicBun.Spawn}");
             Console.WriteLine($"Bunny Base: {BasicBun.Count}");
 
-            while (BasicBun.Count < 7874965825){
-                Console.Write($"Current Buns:{BasicBun.Count}, ");
-                BasicBun.Count = BasicBun.Count + (BasicBun.Count*(long)Math.Round(BasicBun.Spawn));
-                Console.Write($"increased to: {BasicBun.Count} \n");
+            while ( BunTotal < 7874965825){
+
+
+                for( int LoopCount = 0; LoopCount <= 100; LoopCount++){
+
+                    Console.Write($"Current Buns:{BunTotal}, ");
+
+                    BunTotal = BunTotal + (
+                    BasicBun.Count*(long)Math.Round(BasicBun.Spawn) +
+                    SuperBun.Count*(long)Math.Round(SuperBun.Spawn)
+                    );
+
+                    Console.Write($"increased to: {BunTotal} \n");
+
+                    if(LoopCount == 100){
+
+                        Console.WriteLine($"You have {BasicBun.Count} Basic bunnies and {SuperBun.Count} Super bunnies. How many Super Buns would you like?");
+                        SuperBun.Count = SuperBun.Count + long.Parse(Console.ReadLine());
+
+                    }
+
+                }
+
             }
         }
     }
